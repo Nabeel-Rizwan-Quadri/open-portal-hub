d61ee03c9ee3bc85aefe34fe54dc55bb85eb6c37
Nabeel-Rizwan-Quadri <mr.nabeelrizwan@gmail.com>
2025-01-06 16:13:39 +0500

    Updated meeting link to iframe

--- src/components/ui/CalendlyWidget.tsx
+++ src/components/ui/CalendlyWidget.tsx
@@ -25,18 +25,33 @@ const CalendlyWidget = () => {
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
+  useEffect(() => {
+    // Dynamically load Calendly script
+    const script = document.createElement("script");
+    script.src = "https://assets.calendly.com/assets/external/widget.js";
+    script.async = true;
+    document.body.appendChild(script);
+
+    // Clean up script on component unmount
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
   return (
     <>
       <section className="py-20 px-4 gradient-bg">
         <div className="md:container mx-auto text-center">
           <h2 className="text-3xl md:text-4xl font-bold">
-            Book a Meeting Now 
+            Book a Meeting Now
           </h2>
-          <div
-            className={`calendly-inline-widget min-w-80 ${isScreenSmall && "h-[66rem]"} ${isScreenSmallTab && "h-[70rem]"} ${isScreenMedium && "h-[70rem]"}  ${isScreenLarge && "h-[50rem]"} `}
+          <iframe
+            // className="calendly-inline-widget "
+            className={`calendly-inline-widget min-w-80 w-full ${isScreenSmall && "h-[66rem]"} ${isScreenSmallTab && "h-[70rem]"} ${isScreenMedium && "h-[70rem]"}  ${isScreenLarge && "h-[50rem]"} `}
             data-url="https://calendar.app.google/aoU5qv1gX6ocHnAH8"
-            // style={{ minWidth: 320, height: 700 }}
-          ></div>
+            src="https://calendar.app.google/aoU5qv1gX6ocHnAH8"
+          // style={{ minWidth: 320, height: 700 }}
+          />
         </div>
       </section >
     </>
