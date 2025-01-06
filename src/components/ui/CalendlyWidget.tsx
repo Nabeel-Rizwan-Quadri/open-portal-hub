0735dbed3af77572d8d4d162e8b3a7c848cee2a3
Nabeel-Rizwan-Quadri <mr.nabeelrizwan@gmail.com>
2025-01-06 19:13:09 +0500

    Updated to new meet embed code

--- src/components/ui/CalendlyWidget.tsx
+++ src/components/ui/CalendlyWidget.tsx
@@ -43,14 +43,9 @@ const CalendlyWidget = () => {
       <section className="py-20 px-4 gradient-bg">
         <div className="md:container mx-auto text-center">
           <h2 className="text-3xl md:text-4xl font-bold">
-            Book a Meeting Now 
+            Book a Meeting Now
           </h2>
-          <div
-            // className="calendly-inline-widget "
-            className={`calendly-inline-widget min-w-80 ${isScreenSmall && "h-[66rem]"} ${isScreenSmallTab && "h-[70rem]"} ${isScreenMedium && "h-[70rem]"}  ${isScreenLarge && "h-[50rem]"} `}
-            data-url="https://calendar.app.google/aoU5qv1gX6ocHnAH8"
-            // style={{ minWidth: 320, height: 700 }}
-          ></div>
+          <iframe className={`calendly-inline-widget min-w-80 w-full ${isScreenSmall && "h-[66rem]"} ${isScreenSmallTab && "h-[70rem]"} ${isScreenMedium && "h-[70rem]"}  ${isScreenLarge && "h-[50rem]"} `} src="https://calendar.google.com/calendar/appointments/schedules/AcZssZ3uQ_PUYcVYRPtxpvIVUac6CKOKpr9z-lBo3vc-ScYGaeX1766BKtaol1MUfGlZNZ1yU2WLQGPO?gv=true" ></iframe>
         </div>
       </section >
     </>
